61c7260a00feb9facda69c4e8985f8d9d5cc1f01
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2023-11-29 15:50:43 -0400

    update & debugging code

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, model,Types  } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 //reaction schema, subdocument of Thought model.
 const reactionSchema = new Schema(
