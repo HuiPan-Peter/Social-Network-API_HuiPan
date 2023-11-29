43c1cfc48e20d83e301e876244161bb19edc11fb
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2023-11-29 15:07:44 -0400

    Adding DB models

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model,Types  } = require('mongoose');
+
+//reaction schema, subdocument of Thought model.
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: date => new Date(date).toLocaleString()
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+// thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+      minLength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: date => new Date(date).toLocaleString()
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+// create the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
